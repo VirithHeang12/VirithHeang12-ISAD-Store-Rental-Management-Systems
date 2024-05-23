d205e6bacf22603d1228002db567fb22dcbc1499
Heang Virith <virith.id@gmail.com>
2024-05-23 21:10:09 +0700

    init project

---
+++ Store-Rental-Management-Systems/Form1.cs
@@ -0,0 +1,10 @@
+namespace Store_Rental_Management_Systems
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}

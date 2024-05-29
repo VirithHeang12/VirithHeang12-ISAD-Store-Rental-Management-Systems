6df51cca989f5e71d8ea58fb04eab12474f04784
Manin1903 <sokmanin.1918@gmail.com>
2024-05-29 22:13:27 +0700

    commit for design form done

---
+++ Store-Rental-Management-Systems/FrmContract.Designer.cs
@@ -0,0 +1,426 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Forms;
+
+namespace Store_Rental_Management_Systems
+{
+    partial class FrmContract
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label1 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            label6 = new Label();
+            textBox4 = new TextBox();
+            label7 = new Label();
+            label8 = new Label();
+            comboBox1 = new ComboBox();
+            label9 = new Label();
+            comboBox3 = new ComboBox();
+            label10 = new Label();
+            comboBox4 = new ComboBox();
+            label11 = new Label();
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            textBox5 = new TextBox();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Blue;
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(-1, 1);
+            panel1.Margin = new Padding(3, 4, 3, 4);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(861, 85);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Logo;
+            pictureBox1.Location = new Point(3, 0);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(91, 81);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 21;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(125, 48);
+            label2.Name = "label2";
+            label2.Size = new Size(292, 20);
+            label2.TabIndex = 2;
+            label2.Text = "@ Copyright ISAD_SLS_G25 GROUP3(2024)";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(117, 3);
+            label1.Name = "label1";
+            label1.Size = new Size(302, 41);
+            label1.TabIndex = 1;
+            label1.Text = "CONTRACT'S FORM";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = SystemColors.ActiveCaptionText;
+            label3.Location = new Point(12, 269);
+            label3.Name = "label3";
+            label3.Size = new Size(191, 28);
+            label3.TabIndex = 3;
+            label3.Text = "Search (Contract ID) ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Siemreap", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(13, 130);
+            label4.Name = "label4";
+            label4.Size = new Size(101, 27);
+            label4.TabIndex = 4;
+            label4.Text = "ContractID \r\n";
+            // 
+            // textBox1
+            // 
+            textBox1.AccessibleRole = AccessibleRole.None;
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Location = new Point(150, 130);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(186, 27);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.AccessibleRole = AccessibleRole.None;
+            textBox2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Location = new Point(150, 193);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(183, 27);
+            textBox2.TabIndex = 7;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Siemreap", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(13, 193);
+            label5.Name = "label5";
+            label5.Size = new Size(122, 27);
+            label5.TabIndex = 6;
+            label5.Text = "ContractDate ";
+            label5.Click += label5_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.AccessibleRole = AccessibleRole.None;
+            textBox3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox3.BorderStyle = BorderStyle.FixedSingle;
+            textBox3.Location = new Point(606, 193);
+            textBox3.Margin = new Padding(3, 4, 3, 4);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(183, 27);
+            textBox3.TabIndex = 9;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Siemreap", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(466, 193);
+            label6.Name = "label6";
+            label6.Size = new Size(137, 27);
+            label6.TabIndex = 8;
+            label6.Text = "LeaseStartDate";
+            // 
+            // textBox4
+            // 
+            textBox4.AccessibleRole = AccessibleRole.None;
+            textBox4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox4.BorderStyle = BorderStyle.FixedSingle;
+            textBox4.Location = new Point(606, 130);
+            textBox4.Margin = new Padding(3, 4, 3, 4);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(183, 27);
+            textBox4.TabIndex = 11;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Siemreap", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(466, 130);
+            label7.Name = "label7";
+            label7.Size = new Size(131, 27);
+            label7.TabIndex = 10;
+            label7.Text = "LeaseEndDate";
+            // 
+            // label8
+            // 
+            label8.Location = new Point(0, 0);
+            label8.Name = "label8";
+            label8.Size = new Size(114, 31);
+            label8.TabIndex = 35;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Location = new Point(0, 0);
+            comboBox1.Margin = new Padding(3, 4, 3, 4);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(138, 28);
+            comboBox1.TabIndex = 34;
+            // 
+            // label9
+            // 
+            label9.Location = new Point(0, 0);
+            label9.Name = "label9";
+            label9.Size = new Size(114, 31);
+            label9.TabIndex = 33;
+            // 
+            // comboBox3
+            // 
+            comboBox3.Location = new Point(0, 0);
+            comboBox3.Margin = new Padding(3, 4, 3, 4);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(138, 28);
+            comboBox3.TabIndex = 31;
+            // 
+            // label10
+            // 
+            label10.Location = new Point(0, 0);
+            label10.Name = "label10";
+            label10.Size = new Size(114, 31);
+            label10.TabIndex = 32;
+            // 
+            // comboBox4
+            // 
+            comboBox4.Location = new Point(0, 0);
+            comboBox4.Margin = new Padding(3, 4, 3, 4);
+            comboBox4.Name = "comboBox4";
+            comboBox4.Size = new Size(138, 28);
+            comboBox4.TabIndex = 29;
+            // 
+            // label11
+            // 
+            label11.Location = new Point(0, 0);
+            label11.Name = "label11";
+            label11.Size = new Size(114, 31);
+            label11.TabIndex = 30;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = SystemColors.GradientActiveCaption;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(13, 353);
+            dataGridView1.Margin = new Padding(3, 4, 3, 4);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(776, 363);
+            dataGridView1.TabIndex = 21;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Red;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(725, 318);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(64, 31);
+            button1.TabIndex = 22;
+            button1.Text = "Search";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox5
+            // 
+            textBox5.AccessibleRole = AccessibleRole.None;
+            textBox5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox5.BorderStyle = BorderStyle.FixedSingle;
+            textBox5.Location = new Point(13, 318);
+            textBox5.Margin = new Padding(3, 4, 3, 4);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(706, 27);
+            textBox5.TabIndex = 23;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ActiveCaption;
+            button3.FlatStyle = FlatStyle.Popup;
+            button3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.ForeColor = SystemColors.ButtonHighlight;
+            button3.Location = new Point(222, 753);
+            button3.Margin = new Padding(3, 4, 3, 4);
+            button3.Name = "button3";
+            button3.Size = new Size(98, 43);
+            button3.TabIndex = 25;
+            button3.Text = "Update";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ActiveCaption;
+            button4.FlatStyle = FlatStyle.Popup;
+            button4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.ForeColor = SystemColors.ButtonHighlight;
+            button4.Location = new Point(105, 753);
+            button4.Margin = new Padding(3, 4, 3, 4);
+            button4.Name = "button4";
+            button4.Size = new Size(98, 43);
+            button4.TabIndex = 26;
+            button4.Text = "Insert";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.ActiveCaption;
+            button5.FlatStyle = FlatStyle.Popup;
+            button5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.ForeColor = SystemColors.ButtonHighlight;
+            button5.Location = new Point(457, 753);
+            button5.Margin = new Padding(3, 4, 3, 4);
+            button5.Name = "button5";
+            button5.Size = new Size(98, 43);
+            button5.TabIndex = 27;
+            button5.Text = "Clear";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = SystemColors.ActiveCaption;
+            button6.FlatStyle = FlatStyle.Popup;
+            button6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button6.ForeColor = SystemColors.ButtonHighlight;
+            button6.Location = new Point(341, 753);
+            button6.Margin = new Padding(3, 4, 3, 4);
+            button6.Name = "button6";
+            button6.Size = new Size(98, 43);
+            button6.TabIndex = 36;
+            button6.Text = "Delete";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // FrmContract
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Gainsboro;
+            ClientSize = new Size(839, 920);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(textBox5);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Controls.Add(comboBox4);
+            Controls.Add(label11);
+            Controls.Add(comboBox3);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(comboBox1);
+            Controls.Add(label8);
+            Controls.Add(textBox4);
+            Controls.Add(label7);
+            Controls.Add(textBox3);
+            Controls.Add(label6);
+            Controls.Add(textBox2);
+            Controls.Add(label5);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(panel1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "FrmContract";
+            Text = "SaleTicket's Form";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label2;
+        private Label label1;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label5;
+        private TextBox textBox3;
+        private Label label6;
+        private TextBox textBox4;
+        private Label label7;
+        private Label label8;
+        private ComboBox comboBox1;
+        private Label label9;
+        private ComboBox comboBox3;
+        private Label label10;
+        private ComboBox comboBox4;
+        private Label label11;
+        private PictureBox pictureBox1;
+        private DataGridView dataGridView1;
+        private Button button1;
+        private TextBox textBox5;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+    }
+}
---
+++ Store-Rental-Management-Systems/FrmContract.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Store_Rental_Management_Systems
+{
+    public partial class FrmContract : Form
+    {
+        public FrmContract()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
